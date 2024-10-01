38fe45c4de09d63d3a04dccaa64f7b6fe200e9df
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-10-01 21:42:52 +0700

    coba

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'].
 // 						str_replace(basename($_SERVER['SCRIPT_NAME']),"",
 // 						$_SERVER['SCRIPT_NAME']);
-$config['base_url'] = "https://portalberita.demogmt.online/";
+$config['base_url'] = "https://portalberita.demogmt.online/"; 
 
 /*
 |--------------------------------------------------------------------------
